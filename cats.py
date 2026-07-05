a46272bdae09dca5a8e22b3c20954baf2cf4b35a
Ian Buchanan <buchanan_ian@icloud.com>
2026-07-05 14:59:32 +0100

    Added persistent storage using a text file

--- cats.py
+++ cats.py
@@ -9,14 +9,19 @@ else:
     print(f"{input_name} is not in the list of cats.")
 
 #Now we're going to ask the user if they would like to add a new cat to the list.
-#If they say yes, we'll prompt them for the new cat's name and add it to the list. 
-#If they say no, we'll simply print the final list of cats.
+#If they say yes, we'll ask them for the new cat's name and add it to the list. 
+#If they say no, we'll print the final list of cats.
 
 add_cat = input("Would you like to add a new cat to the list? (yes/no): ")
 if add_cat.lower() == "yes":
     new_cat = input("Enter the name of the new cat: ")
     cats.append(new_cat)
-    print(f"{new_cat} has been added to the list of cats.")
+cat_file = open("cats.txt", "w")
+for cat in cats:
+    cat_file.write(cat + "\n")
+    
+cat_file.close()
+print(f"{new_cat} has been added to the list of cats.")
 print("Final list of cats:")
 for cat in cats:
     print(cat)
